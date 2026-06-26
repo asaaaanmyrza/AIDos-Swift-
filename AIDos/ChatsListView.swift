27c79e8f0e132b6de6e0f10639d12ccded5d18e2
Asan Myrzakhmetov <156882836+asaaaanmyrza@users.noreply.github.com>
2026-06-26 20:23:01 +0500

    Добавлен logout() Закончил ВьюМодель чата. loadMessages() и sendMessage() работают исправно

---
+++ AIDos/ChatsListView.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListView.swift
+//  AIDos
+//
+//  Created by Асан Мырзахметов on 24.06.2026.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    @
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ChatListView()
+}
